ad4419cc19165a5e3882f07b5714a8053124d2ef
Иван Мирошников <miro@digitalwand.ru>
2016-09-09 18:10:45 +0300

    ховер карты

--- src/markup/components/footer/footer.js
+++ src/markup/components/footer/footer.js
@@ -1,13 +1,29 @@
 var
     $trees = $('.map-tree'),
-    $placeInput = $('#place-number');
+    $placeInput = $('#place-number'),
+    $map = $('.map'),
+    $mapPopup = $('.j-map-popup'),
+    $mapBtn = $('.j-map-btn'),
+    $mapNum = $('.j-map-num'),
+    num;
 
-$trees.click(function() {
+$trees.mouseenter(function(e) {
     if ( !$(this).hasClass('map-tree_disabled') ) {
-        var place = $(this).attr('data-num');
-        $placeInput.val(place);
-    }
-    else {
-        alert('К сожалению, данное место уже занято. Но вы можете выбрать другое.');
+        var
+            $this = $(this);
+
+        num = $this.attr('data-num');
+
+        $mapNum.html(num);
+        $mapPopup.show();
+        $mapPopup.css('top', $(this).position().top ).css('left', $(this).position().left);
     }
 });
+$map.mouseleave(function(e) {
+    $mapPopup.hide();
+});
+
+$mapBtn.click(function() {
+    $placeInput.val(num);
+});
+
